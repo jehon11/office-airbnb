1fd584cfd618a60e6cfc2cd4b5966bffa564a92e
yves labidoire <yves_labidoire@yahoo.com>
2018-11-13 11:00:02 +0900

    controllers created and populated

---
+++ app/controllers/office_spaces_controller.rb
@@ -0,0 +1,51 @@
+class OfficeSpacesController < ApplicationController
+  def index
+    @office_spaces = OfficeSpace.all
+  end
+
+  def show
+    @office_space = OfficeSpace.find(params[:id])
+  end
+
+  def new
+    @office_space = OfficeSpace.new
+  end
+
+  def create
+    @office_space = OfficeSpace.new(office_space_params)
+    if @office_space.save
+      redirect_to office_space_path(@office_space)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @office_space = OfficeSpace.find(params[:id])
+  end
+
+  def update
+    @office_space = OfficeSpace.find(params[:id])
+    if @office_space.update(office_space_params)
+      redirect_to office_space_path(@office_space)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @office_space = OfficeSpace.find(params[:id])
+    @office_space.destroy
+    redirect_to office_spaces_path
+  end
+
+  private
+
+  def office_space_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:office_space).permit(:name, :address, :description, :price, :photo, :nearest_station)
+  end
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  # root to: 'office_spaces#index'
-  # resources :office_spaces do
-  #   resources :reservations, only: [:new, :create]
-  # end
-  # get 'profile', to: 'pages#profile'
-  # resources :reservations, only: [:delete]
+  root to: 'office_spaces#index'
+  resources :office_spaces do
+    resources :reservations, only: [:new, :create]
+  end
+  get 'profile', to: 'pages#profile'
+  resources :reservations, only: [:delete]
 end
 # branching test
