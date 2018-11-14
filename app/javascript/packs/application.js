250a43d43802ff0045f780dfdf1b9320b5e84b21
yves labidoire <yves_labidoire@yahoo.com>
2018-11-14 16:16:12 +0900

    fix yarn merge bug

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,5 @@
 import "bootstrap";
+import "../plugins/flatpickr"
 console.log('Hello World from Webpacker')
 import { loadDynamicBannerText } from '../components/banner';
 loadDynamicBannerText();
-import "../plugins/flatpickr"
