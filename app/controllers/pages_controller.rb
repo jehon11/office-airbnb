c51e043d166fb70c375650ea198cfad3809940d0
yves labidoire <yves_labidoire@yahoo.com>
2018-11-14 10:42:02 +0900

    beforerebase

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
-  def my_offices
-    @office_spaces = OfficeSpace.where(owner: current_user)
+
+
+  def my_reservations
+    @reservations = Reservation.where(user: current_user)
   end
+
+
 end
