74409367d8a35c08a8d360fa3cb7f6da3fb0a1dc
yves labidoire <yves_labidoire@yahoo.com>
2018-11-15 17:57:15 +0900

    EOD

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,7 @@
 import "bootstrap";
 import "../plugins/flatpickr"
 console.log('Hello World from Webpacker')
+import { bindSweetAlertButtonDemo } from '../components/alert';
+bindSweetAlertButtonDemo();
 import { loadDynamicBannerText } from '../components/banner';
 loadDynamicBannerText();
