7793038e1123375a6c5b9e215eb513f7b30742ff
Jeremy Hon <jehon11@gmail.com>
2018-11-16 09:22:49 +0900

    add review stars and formatted reserve form

--- app/models/office_space.rb
+++ app/models/office_space.rb
@@ -14,4 +14,25 @@ class OfficeSpace < ApplicationRecord
     using: {
       tsearch: { prefix: true } # <-- now `superman batm` will return something!
     }
+
+  def average_rating
+    sum = 0
+    reviews.each do |review|
+      sum += review.rating unless review.rating.nil?
+    end
+    if sum.fdiv(reviews.length).nan?
+      ''
+    else
+      sum.fdiv(reviews.length).fdiv(0.5).ceil * 0.5
+    end
+  end
+
+  def stars
+    average_rating == '' ? 0 : average_rating.floor
+  end
+
+  def half_star
+    average_rating == '' ? '' : !(average_rating % 1 == 0)
+  end
+
 end
