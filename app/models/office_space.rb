5c928688d8e8de66cd1bd99fa43e09ea0f756733
Jeremy Hon <jehon11@gmail.com>
2018-11-17 17:27:11 +0900

    changed dependencies and map styling

--- app/models/office_space.rb
+++ app/models/office_space.rb
@@ -1,7 +1,7 @@
 class OfficeSpace < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
   mount_uploader :photo, PhotoUploader
   belongs_to :owner, class_name: "User", foreign_key: "user_id"
   has_many :users, through: :reservations
